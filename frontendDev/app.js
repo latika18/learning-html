64cf6de5bb8441a00a3fb2a28e510881f25ba6c0
latika18 <34892423+latika18@users.noreply.github.com>
2018-07-08 23:34:38 +0530

    Update app.js

--- frontendDev/app.js
+++ frontendDev/app.js
@@ -5,12 +5,12 @@ $(function(){
         function toggleInfoPanel(anchor) {
           var infoPanelEl = $(this).siblings('.menu-item-info');
           infoPanelEl.toggleClass('is-visible');
-        }
+       }
 
         $('.menu-item > a')
           .mouseover(toggleInfoPanel)
           .mouseout(toggleInfoPanel);
-    }
+   }
 
 
 });
