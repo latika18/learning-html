49949c122a1118ae6d2bd59c87f3ced42b482189
latika18 <34892423+latika18@users.noreply.github.com>
2018-09-07 21:22:19 +0530

    Create jQuery_main.js

---
+++ jQuery_main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  alert('loaded');
+
+  var el = document.getElementById('text');
+  console.log("El in Vanilla Js:",el);
+  console.log("El  in jQuery:", $(el));
+
+
+  document.getElementsByClassName('my-input')[0].value = "Value of input";
+  $('.my-input').val("New Input Val");
+
+});
