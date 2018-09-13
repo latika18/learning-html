8687b6f2b776c53f3535031a5c81bb4f45b00a6e
latika18 <34892423+latika18@users.noreply.github.com>
2018-09-13 12:54:16 +0530

    Update js_gquery.js

--- js_gquery.js
+++ js_gquery.js
@@ -1,1 +1,12 @@
 /** Writing your own version of js**/
+/*** Writing your own version of gquery**/
+class gquery (
+  constructor(el){
+    this.elemet = document.getElementsByClassName(el);
+  }
+)
+
+function gquery(el){
+    var element = new gquery(el);
+    return element;
+}
